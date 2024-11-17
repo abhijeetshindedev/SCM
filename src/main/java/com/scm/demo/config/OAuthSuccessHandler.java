1d865bce49e2df2e81dc16f293209b924ac04b9b
abhijeetshinde4555 <abhijeetshinde.dev@gmail.com>
2024-11-17 16:32:46 +0530

    OAuth-Github-Completed

--- src/main/java/com/scm/demo/config/OAuthSuccessHandler.java
+++ src/main/java/com/scm/demo/config/OAuthSuccessHandler.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -36,34 +37,77 @@ public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
         HttpServletResponse response,
         Authentication authentication) throws IOException, ServletException {
         log.info("in OAuthSuccessHandler");
-        // save user data in DB
-        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
-        log.info("user name : " + user.getName());
-        user.getAttributes().forEach((key, value) -> {
-            log.info(key + " : " + value);
-        });
-        String email = user.getAttributes().get("email").toString();
-        User userFromDB = userRepo.findByEmail(email).orElse(null);
-        if (userFromDB != null) {
-            log.info("user already exists");
-                new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
-                // will create one screen to add all remainng details
-                return;
-        }
-        User userToRegister = new User();
-        userToRegister.setName(user.getAttributes().get("name").toString());
-        userToRegister.setEmail(user.getAttributes().get("email").toString());
-        userToRegister.setPassword("Password");
-        userToRegister.setEmailVarified(true);
-        userToRegister.setProvider(Providers.GOOGLE);
-        userToRegister.setProfilePicLink(user.getAttributes().get("picture").toString());
-        userToRegister.setEnabled(true);
-        userToRegister.setProviderUserId(user.getName());
-        userToRegister.setRoleList(List.of(AppConstants.ROLE_USER));
-        log.info("userToRegister : " + userToRegister);
-        userRepo.save(userToRegister);
+
+        //Identfy the provider
+        var oAuth2AuthToken = (OAuth2AuthenticationToken) authentication;
+        String provider = oAuth2AuthToken.getAuthorizedClientRegistrationId();
         
-        log.info("autuhorities : "+user.getAuthorities());
+        log.info("provider : " + provider);
+
+        //if provider is google
+        if (provider.equals("google")) {
+                // log.info("provider is google");
+                // save user data in DB
+            DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+            log.info("user name : " + user.getName());
+            user.getAttributes().forEach((key, value) -> {
+                log.info(key + " : " + value);
+            });
+            String email = user.getAttributes().get("email").toString();
+            User userFromDB = userRepo.findByEmail(email).orElse(null);
+            if (userFromDB != null) {
+                log.info("user already exists");
+                    new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
+                    return;
+            }
+            User userToRegister = new User();
+            userToRegister.setName(user.getAttributes().get("name").toString());
+            userToRegister.setEmail(user.getAttributes().get("email").toString());
+            userToRegister.setPassword("Password");
+            userToRegister.setEmailVarified(true);
+            userToRegister.setProvider(Providers.GOOGLE);
+            userToRegister.setProfilePicLink(user.getAttributes().get("picture").toString());
+            userToRegister.setEnabled(true);
+            userToRegister.setProviderUserId(user.getName());
+            userToRegister.setRoleList(List.of(AppConstants.ROLE_USER));
+            log.info("userToRegister : " + userToRegister);
+            userRepo.save(userToRegister);
+            
+            log.info("autuhorities : "+user.getAuthorities());
+        }
+
+        //if provider is github
+        if (provider.equals("github")) {
+            log.info("provider is github");
+            // save user data in DB
+            DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+            log.info("user name : " + user.getName());
+            user.getAttributes().forEach((key, value) -> {
+                log.info(key + " : " + value);
+            });
+            String email = user.getAttributes().get("email").toString();
+            User userFromDB = userRepo.findByEmail(email).orElse(null);
+            if (userFromDB != null) {
+                log.info("user already exists");
+                    new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
+                    return;
+            }
+            User userToRegister = new User();
+            userToRegister.setName(user.getAttributes().get("name").toString());
+            userToRegister.setEmail(user.getAttributes().get("email").toString());
+            userToRegister.setAbout(user.getAttributes().get("bio").toString());
+            userToRegister.setPassword("Password");
+            userToRegister.setEmailVarified(true);
+            userToRegister.setProvider(Providers.GITHUB);
+            userToRegister.setProfilePicLink(user.getAttributes().get("avatar_url").toString());
+            userToRegister.setEnabled(true);
+            userToRegister.setProviderUserId(user.getName());
+            userToRegister.setRoleList(List.of(AppConstants.ROLE_USER));
+            log.info("userToRegister : " + userToRegister);
+            userRepo.save(userToRegister);
+        }
+
+        // will create one screen to add all remainng details
         new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
     }
 
